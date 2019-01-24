556f6442d84b4d87288c2cce32effc1480f81b4d
MSMunusamy <43171766+MSMunusamy@users.noreply.github.com>
2019-01-24 16:20:21 +0530

    Update 11.c

--- txt_read_process1_and_process2_write/11.c
+++ txt_read_process1_and_process2_write/11.c
@@ -1,1 +1,8 @@
+/*************************************/
+/* Author      :Munusamy M           */
+/* Date        :22-01-2019           */
+/* Filename    :receiver_p6.c        */
+/* Description :Sample text file     */
+/*                                   */
+/*************************************/
 WELCOME TO OUR HONERABLE CM 
