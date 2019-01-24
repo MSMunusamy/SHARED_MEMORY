192a917cf18c224d030da26c563dc15e966b5029
MSMunusamy <43171766+MSMunusamy@users.noreply.github.com>
2019-01-24 16:26:14 +0530

    Update read.h

--- txt_read_process1_and_process2_write/read.h
+++ txt_read_process1_and_process2_write/read.h
@@ -2,8 +2,8 @@
 /* Author      :Munusamy M           */
 /* Date        :22-01-2019           */
 /* Filename    :receiver_p6.c        */
-/* Description :collect all strings  */
-/*             from p1,p2,p3,p4,p5   */
+/* Description :Headers              */
+/*                                   */
 /*************************************/
 
 #ifndef   READ_H
